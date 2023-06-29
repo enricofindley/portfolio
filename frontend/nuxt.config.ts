f51322afc1132c29d8d0f804dcaecbe7bba7a852
enricofindley <enricofindley96@gmail.com>
2023-06-29 14:55:10 +0700

    try deploy nuxt

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  app: {
+    baseURL: "/portfolio/frontend/",
+  },
+  devtools: { enabled: true },
+});
