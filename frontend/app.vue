cc01aef78bac5f96be7da9b51e489920afe19aab
enricofindley <enricofindley96@gmail.com>
2023-06-29 15:07:18 +0700

    add simple changes

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Welcome to My Portfolio Website!</h1>
+    <p>Check out my <nuxt-link to="/portfolio">portfolio</nuxt-link>.</p>
   </div>
 </template>
