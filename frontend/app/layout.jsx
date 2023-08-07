fa2789637659c4fee707eb79bc4e05cbf2f45e6a
enricofindley <enricofindley96@gmail.com>
2023-08-07 12:10:13 +0700

    add metadata

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -3,8 +3,21 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Enrico Findley | Software Engineer | Data Scientist",
-  description: "Enrico Findley's personal website",
+  title: "Enrico Findley",
+  description: "Enrico Findley's Portfolio Website",
+  generator: "Next.js",
+  applicationName: "Portfolio",
+  referrer: "origin-when-cross-origin",
+  keywords: ["Software Engineer", "Enrico Findley", "Enrico", "Findley", "Portfolio", "Projects", "Data Scientist"],
+  authors: [{ name: "Enrico Findley" }],
+  colorScheme: "dark",
+  themeColor: "#171212",
+  viewport: "width=device-width, initial-scale=1.0",
+  robots: "index, follow",
+  googlebot: "index, follow",
+  google: "notranslate",
+  language: "English",
+  lang: "en",
 };
 
 export default function RootLayout({ children }) {
