e51a95b3a880dbf2402b0b6d9ce869d94b9f3495
enricofindley <enricofindley96@gmail.com>
2023-08-07 13:23:18 +0700

    Improve SEO

---
+++ frontend/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: "https://enricofindley.my.id/sitemap.xml",
+  };
+}
