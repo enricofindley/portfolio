c76642b23bc907fd62dc421d7ae8f6f6310d90c4
enricofindley <enricofindley96@gmail.com>
2023-08-07 13:30:07 +0700

    improve seo

--- frontend/app/robots.js
+++ frontend/app/robots.js
@@ -1,6 +1,4 @@
-import { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
+export default function robots() {
   return {
     rules: {
       userAgent: "*",
